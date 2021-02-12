efca5fc6e22a7ffb6b26cf4d61a4c44254416c2c
Julien <forticolers@gmail.com>
2021-02-12 13:24:09 +0100

    test

---
+++ programming/Datas/src/main/java/utils/QueueCirculaire.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import ch.jeanbourquj.cifom.Data;
+
+/**
+ *
+ * @author JeanbourquJ
+ */
+public class QueueCirculaire {
+
+    public QueueCirculaire(int tailleQueueRef) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean etreVide() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void ajouter(Data data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void retirer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Data retournerValeur() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean etrePlein() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
