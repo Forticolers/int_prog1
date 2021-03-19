99ecd30b3c34cffdf37a8ab048faa85a2fc10010
Julien <forticolers@gmail.com>
2021-03-19 15:42:58 +0100

    LinkedList

--- programming/Datas/src/main/java/ch/jeanbourquj/cifom/utils/LinkedList.java
+++ programming/Datas/src/main/java/ch/jeanbourquj/cifom/utils/LinkedList.java
@@ -13,6 +13,39 @@ import java.util.Objects;
  */
 public class LinkedList implements List {
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.first);
+        hash = 97 * hash + Objects.hashCode(this.last);
+        hash = 97 * hash + this.size;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LinkedList other = (LinkedList) obj;
+        if (this.size != other.size) {
+            return false;
+        }
+        if (!Objects.equals(this.first, other.first)) {
+            return false;
+        }
+        if (!Objects.equals(this.last, other.last)) {
+            return false;
+        }
+        return true;
+    }
+
     /**
      *
      */
