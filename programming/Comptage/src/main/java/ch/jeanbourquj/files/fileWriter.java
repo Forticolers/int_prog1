9e7b237fc0abb2eff8544552a2a08953a7b058c3
Jeanbourquin Julien <JeanbourquJ@s2.rpn.ch>
2021-04-28 11:55:16 +0200

    test

---
+++ programming/Comptage/src/main/java/ch/jeanbourquj/files/fileWriter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.jeanbourquj.files;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ *
+ * @author JeanbourquJ
+ */
+public class fileWriter implements Closeable{
+
+    @Override
+    public void close() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
